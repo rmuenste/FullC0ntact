cafe6456c7d2987c6be8866112e006eb54157fa6
Raphael Muenster <raphael.muenster@math.uni-dortmund.de>
2012-03-22 00:39:56 +0100

    identifier "rad2" seems to be reserved for VC10 compiler

--- inshape3dcore/fortrancppinterface/cppinterface.cpp
+++ inshape3dcore/fortrancppinterface/cppinterface.cpp
@@ -553,7 +553,7 @@ extern "C" void logposition()
 
 extern "C" void logvelocity()
 {
-  i3d::Real rad2 = myWorld.m_vParticles[0].m_dRadius + myWorld.m_vParticles[1].m_dRadius;
+  i3d::Real rad = myWorld.m_vParticles[0].m_dRadius + myWorld.m_vParticles[1].m_dRadius;
   i3d::Real dist = fabs((myWorld.m_vParticles[1].m_vMeshes[0].m_vOrigin-myWorld.m_vParticles[0].m_vMeshes[0].m_vOrigin).mag()-rad2);
   mylog.Write("%f %f",dist,myTimeControl.GetTime());
 }
@@ -562,7 +562,7 @@ extern "C" void logvelocity()
 
 extern "C" void logangularvelocity()
 {
-  i3d::Real rad2 = myWorld.m_vParticles[0].m_dRadius + myWorld.m_vParticles[1].m_dRadius;
+  i3d::Real rad = myWorld.m_vParticles[0].m_dRadius + myWorld.m_vParticles[1].m_dRadius;
   i3d::Real dist = fabs((myWorld.m_vParticles[1].m_vMeshes[0].m_vOrigin-myWorld.m_vParticles[0].m_vMeshes[0].m_vOrigin).mag()-rad2);
   mylog.Write("%f %f",dist,myTimeControl.GetTime());
 }
@@ -571,7 +571,7 @@ extern "C" void logangularvelocity()
 
 extern "C" void logdistance()
 {
-  i3d::Real rad2 = myWorld.m_vParticles[0].m_dRadius + myWorld.m_vParticles[1].m_dRadius;
+  i3d::Real rad = myWorld.m_vParticles[0].m_dRadius + myWorld.m_vParticles[1].m_dRadius;
   i3d::Real dist = fabs((myWorld.m_vParticles[1].m_vMeshes[0].m_vOrigin-myWorld.m_vParticles[0].m_vMeshes[0].m_vOrigin).mag()-rad2);
   mylog.Write("%f %f",dist,myTimeControl.GetTime());
 }
