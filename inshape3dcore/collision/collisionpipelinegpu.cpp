02c617f77741875ef0f54493b04211eda0c254af
superman <raphael.muenster@math.tu-dortmund.de>
2012-11-15 16:37:13 +0100

    added new files for derived class CCollisionPipelineGPU

---
+++ inshape3dcore/collision/collisionpipelinegpu.cpp
@@ -0,0 +1,55 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "collisionpipelinegpu.h"
+#include <world.h>
+#include <iostream>
+#include <timecontrol.h>
+
+namespace i3d {
+
+CCollisionPipelineGPU::CCollisionPipelineGPU()
+{
+
+}
+
+CCollisionPipelineGPU::~CCollisionPipelineGPU()
+{
+	
+}
+
+void CCollisionPipelineGPU::StartPipeline()
+{
+
+  m_pWorld->psystem->update(0.5f);
+
+  //get particles from gpu
+  m_pWorld->psystem->transferArrays(0,m_pWorld->psystem->getNumParticles());
+
+  for(int i=0;i<m_pWorld->m_vRigidBodies.size()-1;i++)
+  {
+    m_pWorld->m_vRigidBodies[i]->m_vCOM.x = m_pWorld->psystem->m_hPos[4*i]; 
+    m_pWorld->m_vRigidBodies[i]->m_vCOM.y = m_pWorld->psystem->m_hPos[4*i+1]; 
+    m_pWorld->m_vRigidBodies[i]->m_vCOM.z = m_pWorld->psystem->m_hPos[4*i+2];
+
+    m_pWorld->m_vRigidBodies[i]->m_vVelocity.x = m_pWorld->psystem->m_hVel[4*i]; 
+    m_pWorld->m_vRigidBodies[i]->m_vVelocity.y = m_pWorld->psystem->m_hVel[4*i+1]; 
+    m_pWorld->m_vRigidBodies[i]->m_vVelocity.z = m_pWorld->psystem->m_hVel[4*i+2];
+  }
+
+}
+
+}
---
+++ inshape3dcore/collision/collisionpipelinegpu.h
@@ -0,0 +1,46 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef COLLISIONPIPELINEGPU_H
+#define COLLISIONPIPELINEGPU_H
+#include <list>
+#include <queue>
+#include <collisionpipeline.h>
+
+namespace i3d {
+
+/**
+* @brief A Collision pipeline represents the sequence of actions in the collision module
+*
+*/
+class CCollisionPipelineGPU : public CCollisionPipeline
+{
+  public:
+    
+	CCollisionPipelineGPU();
+    
+	~CCollisionPipelineGPU();
+
+  /** 
+  * Starts the CD/CR pipeline.
+  */
+  void StartPipeline();
+  
+};
+
+}
+
+#endif // COLLISIONPIPELINEGPU_H
