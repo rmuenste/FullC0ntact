61bed32077931328d72056329a4f891174cb50ea
Raphael Muenster <raphael.muenster@math.uni-dortmund.de>
2013-06-17 17:04:41 +0200

    added new file inshape3dcore/grid/distancemap.h

---
+++ inshape3dcore/grid/distancemap.h
@@ -0,0 +1,50 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <2011>  <Raphael Muenster>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/ 
+#ifndef DISTANCEMAP_H
+#define DISTANCEMAP_H
+
+//===================================================
+//                     INCLUDES
+//===================================================
+#include <list>
+#include <aabb3.h>
+
+namespace i3d {
+
+  
+/**
+ * @brief The class implements a signed distance map based on a uniform grid
+ * 
+ */
+template<class T>  
+class CDistanceMap
+{
+public:
+  CDistanceMap();
+  
+  //cellArray
+  
+  //vertexArray
+  
+  //ClosestPoint to vertex -> easily compute normal
+
+};
+
+}
+#endif
