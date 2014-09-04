489dc19d2f647d16fddacf20f0ee938fb00d0ec8
Raphael Münster <Raphael.muenster@math.uni-dortmund.de>
2014-09-04 08:11:16 +0200

    added new test application

--- applications/test1/test1.cpp
+++ applications/test1/test1.cpp
@@ -3,11 +3,11 @@
 
 namespace i3d {
 
-  class SequentialImpulses : public Application {
+  class Test1 : public Application {
 
   public:
 
-    SequentialImpulses() : Application() {
+    Test1() : Application() {
 
     }
 
@@ -26,21 +26,27 @@ namespace i3d {
       {
         Real simTime = myTimeControl_.GetTime();
         Real energy0 = myWorld_.getTotalEnergy();
+#ifndef FC_SILENT
         std::cout << "------------------------------------------------------------------------" << std::endl;
         std::cout << "## Timestep Nr.: " << myWorld_.timeControl_->m_iTimeStep << " | Simulation time: " << myTimeControl_.GetTime()
           << " | time step: " << myTimeControl_.GetDeltaT() << std::endl;
         std::cout << "Energy: " << energy0 << std::endl;
         std::cout << "------------------------------------------------------------------------" << std::endl;
         std::cout << std::endl;
+#endif        
         int itime=myWorld_.timeControl_->m_iTimeStep;
         myPipeline_.startPipeline();
         Real energy1 = myWorld_.getTotalEnergy();
+#ifndef FC_SILENT
         std::cout << "Energy after collision: " << energy1 << std::endl;
         std::cout << "Energy difference: " << energy0 - energy1 << std::endl;
         std::cout << "Timestep finished... writing vtk." << std::endl;
+#endif
         //if(nOut%20==0)
         Application::writeOutput(nOut,true,true);
+#ifndef FC_SILENT
         std::cout << "Finished writing vtk." << std::endl;
+#endif 
         nOut++;
         myTimeControl_.SetTime(simTime + myTimeControl_.GetDeltaT());
       }//end for
@@ -56,7 +62,7 @@ using namespace i3d;
 int main()
 {
   
-  SequentialImpulses myApp;
+  Test1 myApp;
 
   myApp.init("start/rigidbody.xml");
 
