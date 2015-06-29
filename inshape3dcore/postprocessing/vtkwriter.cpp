5091848a8cb529f788d190ee4f8b4d109db32aa6
Raphael Münster <Raphael.muenster@math.uni-dortmund.de>
2015-06-29 09:49:11 +0200

    Changed output

--- inshape3dcore/postprocessing/vtkwriter.cpp
+++ inshape3dcore/postprocessing/vtkwriter.cpp
@@ -761,22 +761,43 @@ void CVtkWriter::WriteRigidBodies(std::vector<RigidBody*> &pRigidBodies, const c
       }
       pModels.push_back(model_out);
 
-  //       //if the mesh has a bvh call the writetree level method
-  //       if(pMeshObject->m_BVH.GetNumChildren()!=0)
-  //       {
-  //         int depth = pMeshObject->m_BVH.GetDepth();
-  //         for(int level=0;level<depth;level++)
-  //         {
-  //           std::vector<CBoundingVolumeNode3<AABB3r,Real,CTraits> *> vec=
-  //           pMeshObject->m_BVH.GetNodesLevel(level);
-  // 
-  //           std::ostringstream sName;
-  //           std::string sModel(strFileName);
-  //           sName<<".level"<<level;
-  //           sModel.append(sName.str());
-  //           WriteTreeLevel(vec,sModel.c_str());
-  //         }
-  //       }
+      //if the mesh has a bvh call the writetree level method
+      if(pMeshObject->m_BVH.GetNumChildren()!=0)
+      {
+        int depth = pMeshObject->m_BVH.GetDepth();
+        for(int level=0;level<depth;level++)
+        {
+          std::vector<CBoundingVolumeNode3<AABB3r,Real,CTraits> *> vec=
+          pMeshObject->m_BVH.GetNodesLevel(level);
+   
+          std::ostringstream sName;
+          std::string sModel(strFileName);
+          sName<<".level"<<level;
+          sModel.append(sName.str());
+          WriteTreeLevel(vec,sModel.c_str());
+        }
+      }
+
+      //int main(void)
+      //{
+      //  const int dimx = 800, dimy = 800;
+      //  int i, j;
+      //  FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
+      //  (void)fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
+      //  for (j = 0; j < dimy; ++j)
+      //  {
+      //    for (i = 0; i < dimx; ++i)
+      //    {
+      //      static unsigned char color[3];
+      //      color[0] = i % 256;  /* red */
+      //      color[1] = j % 256;  /* green */
+      //      color[2] = (i * j) % 256;  /* blue */
+      //      (void)fwrite(color, 1, 3, fp);
+      //    }
+      //  }
+      //  (void)fclose(fp);
+      //  return EXIT_SUCCESS;
+      //}
       
     }
   }
