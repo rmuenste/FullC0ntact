1c7b47222db7f00f4e22ed3698f741b7f98f3b3c
Raphael Muenster <raphael.muenster@gmail.com>
2016-02-26 15:59:42 +0100

    work on meshmeshtest_gpu

--- cuda_addon/difi.cuh
+++ cuda_addon/difi.cuh
@@ -17,6 +17,7 @@ void sphere_test(i3d::RigidBody *body, i3d::UniformGrid<i3d::Real,i3d::ElementCe
 
 void transfer_uniformgrid(i3d::UniformGrid<i3d::Real,i3d::ElementCell,i3d::VertexTraits<i3d::Real>> *grid);
 void allocate_distancemaps(std::vector<i3d::RigidBody*> &rigidBodies, std::vector<i3d::DistanceMap<i3d::Real>* > &maps);
+void allocate_dmap(i3d::RigidBody* body);
 
 void query_uniformgrid(i3d::RigidBody *body, i3d::UniformGrid<i3d::Real,i3d::ElementCell,i3d::VertexTraits<i3d::Real> > &grid);
 
