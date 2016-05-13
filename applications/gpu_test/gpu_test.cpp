7d2c6a494896bc77e76fe47cc015f79f74afb898
Raphael Muenster <raphael.muenster@gmail.com>
2016-05-13 16:49:01 +0200

    added hashgrid gpu class

--- applications/gpu_test/gpu_test.cpp
+++ applications/gpu_test/gpu_test.cpp
@@ -9,6 +9,7 @@
 #include <3dmodel.h>
 #include <intersectorray3tri3.h>
 #include <perftimer.h>
+#include <difi.cuh>
 
 namespace i3d {
 
@@ -80,7 +81,6 @@ namespace i3d {
           grid_.initCube(xmin_, ymin_, zmin_, xmax_, ymax_, zmax_);
       }
 
-      all_points_dist(grid_);
       //initialize rigid body parameters and
       //placement in the domain
       configureRigidBodies();
@@ -135,119 +135,9 @@ namespace i3d {
 
     void run() {
     
-      RigidBody *body;
-      if (!myWorld_.rigidBodies_.empty())
-      {
-        body = myWorld_.rigidBodies_.front();
-      }
-      else
-      {
-        exit(1);
-      }
-
-      CMeshObject<Real> *meshObject = dynamic_cast< CMeshObject<Real> *>(body->shape_);
-      Model3D *model = &meshObject->m_Model;
-
-      grid_.initStdMesh();
-
-      for (int i = 0; i<dataFileParams_.nTimesteps_; i++)
-      {
-        grid_.refine();
-
-        std::cout << "Generating Grid level" << i + 1 << std::endl;
-        std::cout << "---------------------" << std::endl;
-        std::cout << "NVT=" << grid_.nvt_ << " NEL=" << grid_.nel_ << std::endl;
-        grid_.initStdMesh();
-      }
-
-      my_cuda_func(model, grid_);
-
-      CMeshObject<Real> *object = dynamic_cast< CMeshObject<Real> *>(body->shape_);
-      object->m_BVH.GenTreeStatistics();
-      std::vector<CBoundingVolumeNode3<AABB3r, Real, CTraits> *> leaves =  object->m_BVH.getLeaves();
-      printf("Number of leaves = %i\n", leaves.size());
-
-
-      AABB3f *boxes = new AABB3f[leaves.size()];
-      std::list<int> *triangleIdx = new std::list<int>[leaves.size()];
-      int *sizes = new int[leaves.size()];
-      int i = 0;
-      for (auto &l : leaves)
-      {
-        Vector3<float> v0(l->m_BV.vertices_[0].x, l->m_BV.vertices_[0].y, l->m_BV.vertices_[0].z);
-        Vector3<float> v1(l->m_BV.vertices_[1].x, l->m_BV.vertices_[1].y, l->m_BV.vertices_[1].z);
-        boxes[i].setBox(v0, v1);
-        for (auto &t : l->m_Traits.m_vTriangles)
-        {
-          triangleIdx[i].push_back(t.idx_);
-        }
-        sizes[i] = l->m_Traits.m_vTriangles.size();
-
-        i++;
-      }
-
-      allocateNodes( triangleIdx, boxes, sizes, leaves.size());
-
-      VECTOR3 vQuery(-1.0,0.25,0.030625);
-      int nIntersections = 0;
-      CUnstrGrid::VertexIter ive;
-      int ivt = 65618;
-      CPerfTimer timer;
-      timer.Start();
-      //for (int i = 0; i < grid_.nvt_; i++)
-      ////for (int i = ivt; i <= ivt; i++)
-      //{
-      //  int id = i;
-      //  VECTOR3 vQuery = grid_.vertexCoords_[i];
-      //  //printf("Point [%f,%f,%f] hit with triangle\n", vQuery.x, vQuery.y, vQuery.z);
-
-      //  if (!model->GetBox().isPointInside(vQuery))
-      //  {
-      //    continue;
-      //  }
-
-      //  nIntersections = 0;
-      //  //int id = ive.GetPos();
-      //  //VECTOR3 vQuery((*ive).x, (*ive).y, (*ive).z);
-      //  for (int j = 0; j < model->m_vMeshes[0].m_iNumFaces; j++)
-      //  {
-      //    Triangle3<Real> tri(model->m_vMeshes[0].m_pVertices[model->m_vMeshes[0].m_pFaces[j][0]],
-      //      model->m_vMeshes[0].m_pVertices[model->m_vMeshes[0].m_pFaces[j][1]],
-      //      model->m_vMeshes[0].m_pVertices[model->m_vMeshes[0].m_pFaces[j][2]]);
-
-      //    //determine ray direction
-      //    Vector3<Real> dir(0.0, 0.0, 1.0);/// = vQuery - pNode->m_BV.GetCenter();
-
-      //    Ray3<Real> ray(vQuery, dir);
-      //    CIntersectorRay3Tri3<Real> intersector(ray, tri);
-      //    //test for intersection//
-      //    if (intersector.Intersection())
-      //    {
-      //      //printf("Intersection with triangle: %i \n", j);
-      //      nIntersections++;
-      //    }
-
-      //  }
-      //  if (nIntersections % 2 != 0)
-      //  {
-      //    grid_.m_myTraits[id].iTag = 1;
-      //  }
-      //  else
-      //  {
-      //    grid_.m_myTraits[id].iTag = 0;
-      //  }
-
-      //}
-      //double dt_cpu = timer.GetTime();
-      //printf("CPU time: %3.8f [ms]\n", dt_cpu);
-
-      all_points_test(grid_);
-
-      all_points_dist(grid_);
+      test_hashgrid();
 
       writeOutput(0);
-      writeOutput(1);
-      cleanGPU();
 
     }
 
--- cuda_addon/uniformgrid.cuh
+++ cuda_addon/uniformgrid.cuh
@@ -2,6 +2,57 @@
 
 namespace i3d {
 
+  template<typename T, int memory=gpu>
+    class HashGrid
+    {
+      public:
+
+        unsigned int size_;
+        unsigned int *hashEntries_;
+        unsigned int *particleIndices_;
+
+
+        HashGrid() : size_(0), hashEntries_(nullptr), particleIndices_(nullptr)
+        {
+
+        };
+
+        void sortGrid(unsigned int *dev_indices)
+        {
+          thrust::sort(thrust::device_ptr<unsigned int>(dev_indices), thrust::device_ptr<unsigned int>(dev_indices)+10);
+        }
+
+        void initGrid(unsigned int size, unsigned int *& dev_hash, unsigned int *& dev_indices)
+        {
+
+
+//          unsigned int *dev_hash;
+//          unsigned int *dev_indices;
+
+          cudaMalloc((void**)&dev_hash, size * sizeof(unsigned int));
+          cudaCheckErrors("Copy dev_hash");
+
+          cudaMemcpy(&hashEntries_, &dev_hash, sizeof(unsigned int*), cudaMemcpyHostToDevice); 
+          cudaCheckErrors("Copy dev_hash pointer");
+          
+
+          cudaMalloc((void**)&dev_indices, size * sizeof(unsigned int));
+          cudaCheckErrors("Copy dev_indices");
+
+          cudaMemcpy(&particleIndices_, &dev_indices, sizeof(unsigned int*), cudaMemcpyHostToDevice); 
+          cudaCheckErrors("Copy dev_indices pointer");
+        }
+
+        __device__ void outputInfo()
+        {
+          for(int i(0); i < size_; ++i)
+          {
+            printf("particleIndices_[%i]=%i\n",i,particleIndices_[i]);  
+          }
+        }
+
+    };
+
   template<class T, class CellType>
     class UniformGrid<T,CellType,VertexTraits<T>,gpu>
     {
@@ -65,27 +116,27 @@ namespace i3d {
         }
 
         __device__  
-        void vertexIndices(int icellx,int icelly, int icellz, int indices[8])
-        {
+          void vertexIndices(int icellx,int icelly, int icellz, int indices[8])
+          {
 
-          int dim = dim_[0]+1; 
-          int dim2 = dim * dim; 
-          int baseIndex=icellz*dim2+icelly*dim+icellx; 
+            int dim = dim_[0]+1; 
+            int dim2 = dim * dim; 
+            int baseIndex=icellz*dim2+icelly*dim+icellx; 
 
-          indices[0]=baseIndex;         //xmin,ymin,zmin
-          indices[1]=baseIndex+1;       //xmax,ymin,zmin
+            indices[0]=baseIndex;         //xmin,ymin,zmin
+            indices[1]=baseIndex+1;       //xmax,ymin,zmin
 
-          indices[2]=baseIndex+dim+1; //xmax,ymax,zmin
-          indices[3]=baseIndex+dim;   //xmin,ymax,zmin
+            indices[2]=baseIndex+dim+1; //xmax,ymax,zmin
+            indices[3]=baseIndex+dim;   //xmin,ymax,zmin
 
 
-          indices[4]=baseIndex+dim2;  
-          indices[5]=baseIndex+dim2+1;  
+            indices[4]=baseIndex+dim2;  
+            indices[5]=baseIndex+dim2+1;  
 
-          indices[6]=baseIndex+dim+dim2+1;  
-          indices[7]=baseIndex+dim+dim2;
+            indices[6]=baseIndex+dim+dim2+1;  
+            indices[7]=baseIndex+dim+dim2;
 
-        }
+          }
 
         void query(const Sphere<T> &s)
         {
@@ -122,55 +173,55 @@ namespace i3d {
         }
 
         __device__  
-        void processVertex(const Sphere<T> &s, int cellIndex, int indices[8])
-        {
-          for(int i=0; i < 8; ++i)
+          void processVertex(const Sphere<T> &s, int cellIndex, int indices[8])
           {
-            
-            if((traits_.vertexCoords_[indices[i]] - s.center_).norm2() <= s.radius_*s.radius_)
+            for(int i=0; i < 8; ++i)
             {
-              traits_.fbmVertices_[indices[i]]=1;
-            } 
-            //printf("vertex: %i %i %i\n",indices[i],i,traits_.fbmVertices_[indices[i]]);
-          }
-        } 
+
+              if((traits_.vertexCoords_[indices[i]] - s.center_).norm2() <= s.radius_*s.radius_)
+              {
+                traits_.fbmVertices_[indices[i]]=1;
+              } 
+              //printf("vertex: %i %i %i\n",indices[i],i,traits_.fbmVertices_[indices[i]]);
+            }
+          } 
 
         __device__  
-        void queryVertex(const Sphere<T> &s)
-        {
+          void queryVertex(const Sphere<T> &s)
+          {
 
-          //compute max overlap at level
-          //the max overlap at a level is the maximum distance, 
-          //that an object in the neighbouring cell can penetrate
-          //into the cell under consideration
-          Vector3<T> v = s.center_;
+            //compute max overlap at level
+            //the max overlap at a level is the maximum distance, 
+            //that an object in the neighbouring cell can penetrate
+            //into the cell under consideration
+            Vector3<T> v = s.center_;
 
-          Vector3<T> origin(boundingBox_.center_.x-boundingBox_.extents_[0],
-              boundingBox_.center_.y-boundingBox_.extents_[1],
-              boundingBox_.center_.z-boundingBox_.extents_[2]);
+            Vector3<T> origin(boundingBox_.center_.x-boundingBox_.extents_[0],
+                boundingBox_.center_.y-boundingBox_.extents_[1],
+                boundingBox_.center_.z-boundingBox_.extents_[2]);
 
-          T delta = s.radius_;  
-          T invCellSize = 1.0/cellSize_;  
+            T delta = s.radius_;  
+            T invCellSize = 1.0/cellSize_;  
 
-          int x0=min(int(fmaxf((v.x-delta-origin.x) * invCellSize,0.0)),dim_[0]-1);   
-          int y0=min(int(fmaxf((v.y-delta-origin.y) * invCellSize,0.0)),dim_[1]-1);
-          int z0=min(int(fmaxf((v.z-delta-origin.z) * invCellSize,0.0)),dim_[2]-1);
+            int x0=min(int(fmaxf((v.x-delta-origin.x) * invCellSize,0.0)),dim_[0]-1);   
+            int y0=min(int(fmaxf((v.y-delta-origin.y) * invCellSize,0.0)),dim_[1]-1);
+            int z0=min(int(fmaxf((v.z-delta-origin.z) * invCellSize,0.0)),dim_[2]-1);
 
-          int x1=min(int(fmaxf((v.x+delta-origin.x) * invCellSize,0.0)),dim_[0]-1);   
-          int y1=min(int(fmaxf((v.y+delta-origin.y) * invCellSize,0.0)),dim_[1]-1);   
-          int z1=min(int(fmaxf((v.z+delta-origin.z) * invCellSize,0.0)),dim_[2]-1);   
+            int x1=min(int(fmaxf((v.x+delta-origin.x) * invCellSize,0.0)),dim_[0]-1);   
+            int y1=min(int(fmaxf((v.y+delta-origin.y) * invCellSize,0.0)),dim_[1]-1);   
+            int z1=min(int(fmaxf((v.z+delta-origin.z) * invCellSize,0.0)),dim_[2]-1);   
 
-          //loop over the overlapped cells
-          for(int x=x0;x<=x1;x++)
-            for(int y=y0;y<=y1;y++)
-              for(int z=z0;z<=z1;z++)
-              {
-                int indices[8];
-                vertexIndices(x,y,z, indices);
-                int index = z*dim_[1]*dim_[0]+y*dim_[0]+x;
-                processVertex(s,index, indices);
-              }//for z  
-        }
+            //loop over the overlapped cells
+            for(int x=x0;x<=x1;x++)
+              for(int y=y0;y<=y1;y++)
+                for(int z=z0;z<=z1;z++)
+                {
+                  int indices[8];
+                  vertexIndices(x,y,z, indices);
+                  int index = z*dim_[1]*dim_[0]+y*dim_[0]+x;
+                  processVertex(s,index, indices);
+                }//for z  
+          }
 
         void transferData(const UniformGrid<T,CellType,VertexTraits<T>,cpu> &grid)
         {
