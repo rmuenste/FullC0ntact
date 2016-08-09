ba545921a9a8c8e31de21067970533e14edd4033
Raphael Muenster <raphael.muenster@gmail.com>
2016-08-09 17:52:04 +0200

    corrected spring mass model

--- applications/openmeshtest/openmeshtest.cpp
+++ applications/openmeshtest/openmeshtest.cpp
@@ -19,20 +19,30 @@ namespace i3d {
     T l;
     Vector3<T> L;
     T dt;
-    SpringConstraint() : ks(T(0.5)), kd(T(-0.25)), l0(T(1)), l(T(0)), dt(T(0))
+    SpringConstraint() : ks(T(0.6)), kd(T(-0.25)), l0(T(1)), l(T(0)), dt(T(0))
     {
     }
 
     Vector3<T> evalForce(const Vector3<T> &x0, const Vector3<T> &x1, const Vector3<T> &v0, const Vector3<T> &v1)
     {
-      L = (x1 - x0);
+      L = (x0 - x1);
       l = L.mag();
       if(l == 0.0f)
         return Vector3<T>(0,0,0);
 
       // what if vertices coincide?
       Vector3<T> LN = (1.0 / l) * L;
-      Vector3<T> f = (ks * (l - l0) + kd * ((v0 - v1) * L)) * LN; 
+      T tl = -ks * (l - l0);
+      T tr = kd * ((v0 - v1) * L)/l;
+//      std::cout << "> l: " << l << std::endl;
+//      std::cout << "> L: " << L << std::endl;
+//      std::cout << "> lt: " << tl << std::endl;
+//      std::cout << "> rt: " << tr << std::endl;
+      Vector3<T> f = (tl + tr) * LN; 
+//      std::cout << "> force: " << f << std::endl;
+
+      //Vector3<T> f = (ks * (l - l0) + kd * ((v0 - v1) * L)) * LN; 
+
       return f;
     }
 
@@ -46,7 +56,7 @@ namespace i3d {
     private:
       Point  cog_;
     public:
-      VertexT() : cog_(Point(0.0f, 0.0f, 0.0f)), force_(0,0,0), vel_(0,0,0), pos_old_(0,0,0), mass_(1), fixed_(false) { }
+      VertexT() : cog_(Point(0.0f, 0.0f, 0.0f)), force_(0,0,0), vel_(0,0,0), pos_old_(0,0,0), mass_(0.5), fixed_(false) { }
       const Point& cog() const { return cog_; }
       void set_cog(const Point& _p) { cog_ = _p; }
       Vec3 force_;
@@ -85,7 +95,7 @@ namespace i3d {
 
     void integrate()
     {
-      Real dt = 0.001;
+      Real dt = 1.0/60.0;
       PolyMesh::VertexIter v_it, v_end(polyMesh.vertices_end()); 
       for(v_it = polyMesh.vertices_begin(); v_it!=v_end; ++v_it)
       {
@@ -96,31 +106,35 @@ namespace i3d {
         Vec3 &vel = polyMesh.data(*v_it).vel_; 
 
         Vec3 &force = polyMesh.data(*v_it).force_; 
-        Vec3 g(0,0,-9.81);
+        Vec3 g(0,-0.00981,0);
         force += g;
+
+        force += -0.0125 * vel;
 
         Real &m = polyMesh.data(*v_it).mass_; 
         Vec3 pos = Vec3(polyMesh.point(*v_it)[0], polyMesh.point(*v_it)[1], polyMesh.point(*v_it)[2]);
 
         
-        vel = vel + dt * force * 1.0/m;
+        vel = vel + dt * force * (1.0/m);
 
         polyMesh.data(*v_it).pos_old_ = Vec3(polyMesh.point(*v_it)[0], polyMesh.point(*v_it)[1], polyMesh.point(*v_it)[2]);
 
         pos = pos + dt * vel;
 
-        if(pos.z < -2.0)
-        {
-          pos.z = -2.0;
-          force.z = 0;
-          vel.z = 0.5 * -vel.z;
-        }
+//        if(pos.z < -2.0)
+//        {
+//          pos.z = -2.0;
+//          force.z = 0;
+//          vel.z = 0.5 * -vel.z;
+//        }
+//        std::cout << "> force: " << force << std::endl;
 
         //std::cout << "> old pos: " << polyMesh.data(*v_it).pos_old_ << std::endl;
         //std::cout << "> pos: " << pos << std::endl;
         PolyMesh::Point p(pos.x, pos.y, pos.z);
 
         polyMesh.set_point(v_it, p);
+        force = Vec3(0,0,0);
 
       }
     }
@@ -133,7 +147,7 @@ namespace i3d {
 
 //        std::cout << "> Edge: " << *e_it << std::endl;
 //        std::cout << "> Edge traits: " << polyMesh.data(*e_it).spring_.l0 << std::endl;
-//        std::cout << "> Edge point (0): " << polyMesh.to_vertex_handle(polyMesh.halfedge_handle(*e_it,0)).idx() << std::endl;
+//        std::cout << "> Edge point (0):st " << polyMesh.to_vertex_handle(polyMesh.halfedge_handle(*e_it,0)).idx() << std::endl;
 //        std::cout << "> Edge point (1): " << polyMesh.to_vertex_handle(polyMesh.halfedge_handle(*e_it,1)).idx() << std::endl;
         
         PolyMesh::VertexHandle vh0 = polyMesh.to_vertex_handle(polyMesh.halfedge_handle(*e_it,0));
@@ -168,14 +182,18 @@ namespace i3d {
         polyMesh.data(*e_it).spring_.l0 = (x0-x1).mag();
       }
 
+      std::cout << "> Number of structural springs: " << polyMesh.n_edges() << std::endl;
+      e_it =polyMesh.edges_begin();
+      std::cout << "> Rest length: " << polyMesh.data(*e_it).spring_.l0 << std::endl;
+
       PolyMesh::VertexIter v_it, v_end(polyMesh.vertices_end()); 
       for(v_it = polyMesh.vertices_begin(); v_it!=v_end; ++v_it)
       {
         Vec3 p(polyMesh.point(*v_it)[0], polyMesh.point(*v_it)[1], polyMesh.point(*v_it)[2]);
 
-        std::cout << "> idx: " << (*v_it).idx() << std::endl;
-        std::cout << "> pos: " << p << std::endl;
-        if((*v_it).idx() == 2 || (*v_it).idx() == 3)
+//        std::cout << "> idx: " << (*v_it).idx() << std::endl;
+//        std::cout << "> pos: " << p << std::endl;
+        if((*v_it).idx() == 2 || (*v_it).idx() == 285)
         {
           polyMesh.data(*v_it).fixed_ = true;
         }
@@ -184,7 +202,7 @@ namespace i3d {
 
     void run() {
 
-      int steps = 100;
+      int steps = 10000;
       //start the main simulation loop
 
       mesh_.clear();
@@ -193,7 +211,7 @@ namespace i3d {
       OpenMesh::IO::read_mesh(mesh_, "meshes/engrave.obj");
       std::cout << "> Mesh vertices: " << mesh_.n_vertices() << std::endl;
 
-      OpenMesh::IO::read_mesh(polyMesh, "meshes/cloth_high.obj");
+      OpenMesh::IO::read_mesh(polyMesh, "meshes/cloth2.obj");
       std::cout << "> PolyMesh vertices: " << polyMesh.n_vertices() << std::endl;
       std::cout << "> PolyMesh edges: " << polyMesh.n_edges() << std::endl;
       std::cout << "> PolyMesh faces: " << polyMesh.n_faces() << std::endl;
@@ -212,6 +230,7 @@ namespace i3d {
         std::streamsize            _precision = 6;
         OpenMesh::IO::ExporterT<PolyMesh> exporter(polyMesh);
         writer.write(name.str().c_str(), exporter, _opt, _precision);
+        std::cout << "> Time step: " << istep << std::endl;
 
       }
 
