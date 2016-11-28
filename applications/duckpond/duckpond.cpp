5d3467515043a179d39c8b2fd0fcd9035b5b73f2
Raphael Muenster <raphael.muenster@gmail.com>
2016-11-28 17:18:26 +0100

    tweaked flagella movement

--- applications/duckpond/duckpond.cpp
+++ applications/duckpond/duckpond.cpp
@@ -99,6 +99,14 @@ namespace i3d {
       for(int i(1); i < N_tail; ++i)
       {
         Real x = Real(i) * l0; 
+        // -2.0 * pi * fs * t + q * x < 9/4 * pi
+        // -2.0 * pi * fs * t + q * x >= 9/4 * pi
+        Real xl = -2.0 * pi * fs * t + q * x;
+        A = 3.2;
+  //      if(xl < (9.0/4.0) * pi)
+  //      {
+  //        A = (1.0 - xl/(9.0/4.0)) * 1.5 + (xl/(9.0/4.0)) * 3.2;
+  //      }
         Real y = A * std::sin(-2.0 * pi * fs * t + q * x);
         pl.vertices_.push_back(Vec3(x,y,0)); 
         pl.segments_.push_back(Segment3<Real>(pl.vertices_[i-1], pl.vertices_[i]));
@@ -116,6 +124,12 @@ namespace i3d {
         for(int i(0); i < N_tail; ++i)
         {
           Real x = Real(i) * l0; 
+          Real xl = -2.0 * pi * fs * t + q * x;
+          A = 3.2;
+//          if(xl < (9.0/4.0) * pi)
+//          {
+//            A = (1.0 - xl/(9.0/4.0)) * 1.5 + (xl/(9.0/4.0)) * 3.2;
+//          }
           Real y = A * std::sin(-2.0 * pi * fs * t + q * x);
           pl.vertices_[i]=Vec3(x,y,0); 
         }
