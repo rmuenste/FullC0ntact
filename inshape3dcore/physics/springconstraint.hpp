d942c0754a6e77b0a4e5547c776add701cf3ec3c
Raphael Muenster <raphael.muenster@gmail.com>
2016-11-29 20:22:38 +0100

    Implemented softbody class

--- inshape3dcore/physics/springconstraint.hpp
+++ inshape3dcore/physics/springconstraint.hpp
@@ -10,7 +10,23 @@ namespace i3d
   {
   public:
 
-    SpringConstraint() : ks_(T(0.5)), kd_(T(-0.25)), l0_(T(1)), l_(T(0)), dt_(T(0))
+    T kd_;
+    T ks_;
+    T l0_;
+    T l_;
+    T dt_;
+
+    Vector3<T> L_;
+
+    Vector3<T> *x0_;
+    Vector3<T> *x1_;
+
+    Vector3<T> *v0_;
+    Vector3<T> *v1_;
+
+    int i0_,i1_;
+
+    BasicSpringConstraint() : ks_(T(0.5)), kd_(T(-0.25)), l0_(T(1)), l_(T(0)), dt_(T(0))
     {
 
     }
@@ -20,7 +36,34 @@ namespace i3d
 
     }
 
-    virtual ~BasicSpringConstraint ();
+    BasicSpringConstraint(T _ks, T _kd, T _l0, T _l,   
+                          Vector3<T> *x0, Vector3<T> *x1,
+                          Vector3<T> *v0, Vector3<T> *v1)
+      : ks_(_ks), kd_(_kd), l0_(_l0), l_(_l)
+    {
+
+      x0_ = x0;
+      x1_ = x1;
+
+      v0_ = v0;
+      v1_ = v1;
+
+    }
+
+    BasicSpringConstraint(T _ks, T _kd, T _l0, int _i0, int _i1, 
+                          Vector3<T> *x0, Vector3<T> *x1)
+      : ks_(_ks), kd_(_kd), l0_(_l0), i0_(_i0), i1_(_i1) 
+    {
+
+      x0_ = x0;
+      x1_ = x1;
+
+    }
+
+    virtual ~BasicSpringConstraint ()
+    {
+
+    };
 
     virtual Vector3<T> evalForce()
     {
@@ -37,26 +80,14 @@ namespace i3d
 
       // what if vertices coincide?
       Vector3<T> LN = (1.0 / l_) * L_;
-      T tl = -ks_ * (l_ - l0_);
-      T tr = kd_ * ((v0 - v1) * L_)/l_;
-      Vector3<T> f = (tl + tr) * LN; 
+      T tl          = -ks_ * (l_ - l0_);
+      T tr          = kd_ * ((v0 - v1) * L_)/l_;
+      Vector3<T> f  = (tl + tr) * LN; 
 
       return f;
     }
 
 
-    T kd_;
-    T ks_;
-    T l0_;
-    T l_;
-    Vector3<T> L_;
-    T dt_;
-
-    Vector3<T> *x0_;
-    Vector3<T> *x1_;
-
-    Vector3<T> *v0_;
-    Vector3<T> *v1_;
   
   private:
     /* data */
--- inshape3dcore/softbody.hpp
+++ inshape3dcore/softbody.hpp
@@ -1,38 +1,188 @@
 #ifndef SOFTBODY_HPP_NRX1JCK5
 #define SOFTBODY_HPP_NRX1JCK5
 
+#include <vector3.h>
+#include <segment3.h>
+#include <paramline.h>
+#include <springconstraint.hpp>
+#include <cmath>
+
 namespace i3d
 {
 
-  template <typename T> 
+  template <typename T, class G> 
   class BasicSoftBody
   {
   public:
-    BasicSoftBody ();
+    BasicSoftBody ()
+    {
 
-    virtual ~BasicSoftBody ();
+    };
 
-    virtual void internalForce(T dt); 
+    virtual ~BasicSoftBody ()
+    {
 
-    virtual void applyForce(T dt); 
+    };
+
+    G geom_;
+
+    virtual void internalForce(T t){}; 
+
+    virtual void applyForce(T dt){}; 
+
+    virtual void init(){}; 
+
+    virtual void integrate(){}; 
+    
   
   private:
     /* data */
   };
 
-  template<typename T, typename Geometry>
-  class SoftBody : public BasicSoftBody<T>
+  template<typename T, class G>
+  class SoftBody : public BasicSoftBody<T, G>
   {
   public:
-    SoftBody ();
+    SoftBody ()
+    {
 
-    virtual ~SoftBody ();
+    };
+
+    virtual ~SoftBody ()
+    {
+
+    };
+
+    void internalForce(T t){}; 
+
+    void applyForce(T dt){}; 
+
+    void init(){}; 
+
+    void integrate(){}; 
   
   private:
     /* data */
+  };
+
+  template<>
+  class SoftBody<Real, ParamLine<Real> > : public BasicSoftBody<Real, ParamLine<Real>>
+  {
+    public:
+
+    Real A_;
+
+    Real q_;
+
+    Real N_;
+
+    Real f_;
+
+    Real m_;
+
+    Real k_tail_;
+
+    Real kd_;
+
+    Real ks_;
+
+    Real a0_;
+
+    Real l0_;
+
+    std::vector< BasicSpringConstraint<Real> > springs_;
+
+    SoftBody () : A_(3.2), N_(100),  f_(1.0/120.0), a0_(0.5), l0_(1.0*a0_)
+    {
+
+    };
+
+    virtual ~SoftBody (){};
+
+    void internalForce(Real t)
+    {
+
+      const double pi = 3.1415926535897;
+
+      Real q = 4.0 * pi/(l0_ * N_); 
+
+      int j = 0;
+
+      for(int i(15); i < N_; ++i)
+      {
+
+        Real x = Real(j) * a0_;
+
+        Real xl = -2.0 * pi * f_ * t + q * x;
+
+        Real y = A_ * std::sin(xl);
+
+        geom_.vertices_[i]=Vec3(x,y,0);
+
+        j++;
+
+      }
+
+      // evaluate force for the first 15 segments
+
+    }; 
+
+    void applyForce(Real dt)
+    {
+
+    }; 
+
+    void init()
+    {
+
+
+      a0_ = 0.5;
+
+      const double pi = 3.1415926535897;
+
+      Real q = 4.0 * pi/(l0_ * N_); 
+
+      Real xx = Real(-15) * l0_;
+      Real yy = A_ * std::sin(-2.0 * pi * f_ + q * xx);
+
+      geom_.vertices_.push_back(Vector3<Real>(xx,
+                                              yy,
+                                              0));
+
+      int j = -14; 
+
+      for(int k=1; k < N_; ++k)
+      {
+
+        Real x = Real(j) * l0_;
+
+        Real y = A_ * std::sin(-2.0 * pi * f_ + q * x);
+
+        geom_.vertices_.push_back(Vector3<Real>(x,y,0));
+
+        geom_.faces_.push_back(std::pair<int,int>(k-1,k));
+
+        geom_.segments_.push_back(Segment3<Real>(  geom_.vertices_[k-1], 
+                                                   geom_.vertices_[k]
+                                                  ));
+
+        springs_.push_back(BasicSpringConstraint<Real>(ks_, kd_, l0_, k-1, k,
+                                                       &geom_.vertices_[k-1],
+                                                       &geom_.vertices_[k]));
+
+        j++;
+
+      }
+
+    }; 
+
+    void integrate()
+    {
+      
+    }; 
+
   };
   
 } /* i3d */ 
-
 
 #endif /* end of include guard: SOFTBODY_HPP_NRX1JCK5 */
