7085b957a6709b0c8b942805b99572f9c5298ca1
Raphael Muenster <raphael.muenster@gmail.com>
2017-11-30 23:52:40 +0100

    Added more functions to the cgal interface

--- inshape3dcore/fortrancppinterface/cgal_interface.hpp
+++ inshape3dcore/fortrancppinterface/cgal_interface.hpp
@@ -38,3 +38,175 @@ typedef CGAL::AABB_face_graph_triangle_primitive<Polyhedron> Primitive;
 typedef CGAL::AABB_traits<Kernel, Primitive> Traits;
 typedef CGAL::AABB_tree<Traits> Tree;
 typedef Tree::Point_and_primitive_id Point_and_primitive_id;
+
+
+double random_in(const double a,
+  const double b)
+{
+  double r = rand() / (double)RAND_MAX;
+  return (double)(a + (b - a) * r);
+}
+
+/*
+ * This function generates and returns a
+ * random 3d vector with components x,y,z in [0,1]
+ */
+Vector random_vector()
+{
+  double x = random_in(0.0, 1.0);
+  double y = random_in(0.0, 1.0);
+  double z = random_in(0.0, 1.0);
+  return Vector(x, y, z);
+}
+
+void initGeometry() {
+
+  // only one output file
+  unsigned nOut = 0;
+
+  // Get access to the rigid body wrapper class
+  RigidBody *body = myWorld_.rigidBodies_.front();
+
+  CMeshObjectr *pMeshObject = dynamic_cast<CMeshObjectr *>(body->shape_);
+  std::string objPath = pMeshObject->GetFileName();
+
+  std::cout << "Name of mesh file: " << objPath << std::endl;
+
+
+  for (auto &b : myWorld_.rigidBodies_)
+  {
+
+    if (b->shapeId_ == RigidBody::BOUNDARYBOX)
+      continue;
+
+    // Load a mesh from file in the CGAL off format
+    std::ifstream in(std::string("meshes/lnx.off"));
+
+    if (!in)
+    {
+      std::cerr << "unable to open file" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+    Polyhedron *polyhedron = new Polyhedron();
+
+    // Read the polyhedron from the stream
+    in >> *polyhedron;
+
+    if (!in)
+    {
+      std::cerr << "invalid OFF file" << std::endl;
+      delete polyhedron;
+      polyhedron = nullptr;
+      std::exit(EXIT_FAILURE);
+    }
+
+    in.close();
+
+    polyhedra_.push_back(polyhedron);
+
+    std::cout << "OFF file loaded successfully" << std::endl;
+  }
+
+}
+
+void computePointsInside(RigidBody *body, Tree &tree)
+{
+
+  // Compute the total number of points
+  int total = (body->map_->cells_[0] + 1)*(body->map_->cells_[1] + 1)*(body->map_->cells_[2] + 1);
+
+  // Generate a direction vector for the ray
+  Vector vec = random_vector();
+
+  // Get a timer object and start the timer 
+  // to measure the ray intersection tests
+  CPerfTimer timer;
+  timer.Start();
+
+  // Loop over all points and perform the ray intersection test 
+  // for each point of the mesh
+  for (int i = 0; i < total; i++)
+  {
+    VECTOR3 vQuery = body->map_->vertexCoords_[i];
+
+    Point p(vQuery.x, vQuery.y, vQuery.z);
+
+    Ray ray(p, vec);
+
+    int nb_intersections = (int)tree.number_of_intersected_primitives(ray);
+
+    // Check for odd or even number of intersections
+    if (nb_intersections % 2 != 0)
+      body->map_->stateFBM_[i] = 1;
+    else
+      body->map_->stateFBM_[i] = 0;
+
+  }
+
+  timer.Stop();
+  std::cout << "FBM CGAL: " << timer.GetTime() << "[ms]" << std::endl;
+
+}
+
+void computePointsDistance(RigidBody *body, Tree &tree) {
+
+  // Compute the total number of points
+  int total = (body->map_->cells_[0] + 1)*(body->map_->cells_[1] + 1)*(body->map_->cells_[2] + 1);
+
+  CPerfTimer timer;
+  // Start the timer again to measure the distance calculation
+  timer.Start();
+
+  // Loop over all points and perform distance computation
+  // for each point of the mesh
+  for (int i = 0; i < total; i++)
+  {
+    VECTOR3 vQuery = body->map_->vertexCoords_[i];
+    Point p(vQuery.x, vQuery.y, vQuery.z);
+    Point cp = tree.closest_point(p);
+
+    body->map_->distance_[i] = std::sqrt(CGAL::squared_distance(p, cp));
+
+    // For an inner point we multiply by
+    // -1 to get the signed distance function
+    if (body->map_->stateFBM_[i])
+      body->map_->distance_[i] *= -1.0;
+
+    // also store the point on the surface of the object
+    // with the shortest distance the input mesh point
+    Vec3 contactPoint = pointToVec3(cp);
+    body->map_->contactPoints_[i] = contactPoint;
+
+    // construct a surface normal which may be
+    // needed for other calculations
+    body->map_->normals_[i] = vQuery - contactPoint;
+    body->map_->normals_[i].normalize();
+  }
+
+  timer.Stop();
+  std::cout << "Dist CGAL: " << timer.GetTime() << "[ms]" << std::endl;
+
+}
+
+void buildTreeStructures() {
+
+  std::cout << "Construct AABB tree...";
+
+  // Construct an instance of the CGAL::AABB_tree<Traits> tree
+  // from the polyhedron we have just read in
+  for (auto &polyhedron : polyhedra_)
+  {
+
+    Tree *tree = new Tree(faces(*polyhedron).first, faces(*polyhedron).second, *polyhedron);
+
+    // Use the acceleration method for distances
+    tree->accelerate_distance_queries();
+
+    trees_.push_back(tree);
+
+  }
+
+  std::cout << "done." << std::endl;
+
+}
+
