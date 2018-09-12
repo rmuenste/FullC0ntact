a4e048c544f83c19044b0ae5fbdba8449abfa297
superman <raphael.muenster@math.tu-dortmund.de>
2018-09-12 11:55:08 +0200

    Updated the python plotter

--- libs/pyplot.py
+++ libs/pyplot.py
@@ -10,8 +10,18 @@
 # import libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import sys
 import getopt
+import time
+
+globalFileName = ""
+globalColumns = []
+
+fig, ax = plt.subplots()
+fig2, ax2 = plt.subplots()
+
+line, = ax.plot([],[], 'ro', animated=True)
 
 #===============================================================================
 #                                Usage
@@ -28,12 +38,103 @@ def usage():
 #===============================================================================
 def parseColumns(argin):
     """
-    Parses the input argument <extrusion-layers> that tells us
-    the number of extrusions on each level
+    Parses the input argument <columns> that tells us
+    which columns to use for plotting 
     """
     ex = argin.split(",")
     ex = [int(x) for x in ex]
     return ex
+
+#===============================================================================
+#                      Function: updatePlot
+#===============================================================================
+def plotUpdate(fileName, cols):
+    """
+    Parses the input argument <extrusion-layers> that tells us
+    the number of extrusions on each level
+    """
+    
+    print("Redrawing")
+    in_data=np.loadtxt(fileName)
+
+    ax.clear()
+    for idx in range(0, len(cols), 2):
+        xidx = cols[idx]
+        yidx = cols[idx+1]
+        ax.plot(in_data[:,xidx],in_data[:,yidx])
+#===============================================================================
+#                      Function: updatePlot
+#===============================================================================
+def init():
+    """
+    Parses the input argument <extrusion-layers> that tells us
+    the number of extrusions on each level
+    """
+    
+    print("Init plot")
+    inputData=np.loadtxt("observables.log")
+    xmax = np.max(inputData[:,0])
+    ymax = np.max(inputData[:,1])
+    xmin = np.min(inputData[:,0])
+    ymin = np.min(inputData[:,1])
+
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+    ax.grid()
+    line, = ax.plot([],[], 'ro', animated=True)
+    return line,
+
+#===============================================================================
+#                      Function: updatePlot
+#===============================================================================
+def animate(data):
+    """
+    Parses the input argument <extrusion-layers> that tells us
+    the number of extrusions on each level
+    """
+    
+    ax.clear()
+    ax2.clear()
+    xx, yy, yy2, yy3 = data
+
+
+    ymin = np.min([yy, yy2])
+    ymax = np.max([yy, yy2])
+    #print("min,max:" + str(ymin) + " " + str(ymax))
+    ymin2 = np.min([yy3])
+    ymax2 = np.max([yy3])
+
+    ax.set_xlim([0, xx[len(xx)-1]])
+    ax.set_ylim([ymin, ymax + 0.1 * (ymax - ymin)])
+
+    ax.grid()
+
+    ax2.set_xlim([0, xx[len(xx)-1]])
+    ax2.set_ylim([ymin2, ymax2])
+    ax.set_ylim([ymin2, ymax2])
+
+    ax2.grid()
+
+    line.set_xdata(xx)
+    line.set_ydata(yy2)
+#    ax.plot(xx, yy, lw=2, color="blue")
+#    ax.plot(xx, yy2, lw=2, color="red")
+#    ax2.plot(xx, yy3, lw=2, color="magenta")
+    ax.plot(xx, yy3, lw=2, color="magenta")
+    return line,
+
+#===============================================================================
+#                      Function: updatePlot
+#===============================================================================
+def data_gen(fileName):
+    
+    print("Data Gen using " + fileName)
+    cnt = 0
+    while True: 
+        cnt += 1
+        inputData=np.loadtxt("observables.log")
+        yield inputData[:,0],inputData[:,3],inputData[:,6],inputData[:,15]
+        #yield inputData[:,0],inputData[:,3],inputData[:,6],inputData[:,15]
 
 #===============================================================================
 #                        Main Script Function
@@ -59,8 +160,10 @@ def main():
             sys.exit(2)
         elif opt in ('-f', '--file'):
             file_data = arg
+            globalFileName = file_data
         elif opt in ('-c', '--columns'):
             columns = parseColumns(arg)
+            globalColumns = columns
         else:
             usage()
             sys.exit(2)
@@ -69,13 +172,8 @@ def main():
       usage()
       sys.exit(2)
 
-    in_data=np.loadtxt(file_data)
-
-    for idx in range(0, len(columns), 2):
-        xidx = columns[idx]
-        yidx = columns[idx+1]
-        plt.plot(in_data[:,xidx],in_data[:,yidx])
-
+    #ani = animation.FuncAnimation(fig, animate, data_gen(file_data), blit=False, interval=10000, repeat = False, init_func = init)
+    ani = animation.FuncAnimation(fig, animate, data_gen(file_data), blit=False, interval=10000, repeat = False)
     plt.show()
 
 #===============================================================================
