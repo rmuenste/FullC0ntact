6d66509b397440f50ff1d90902fcdbd3442d6a88
Raphael Muenster <raphael.muenster@gmail.com>
2018-09-20 16:31:05 +0200

    Corrected the gcc version for include header

--- inshape3dcore/fortrancppinterface/io_func.hpp
+++ inshape3dcore/fortrancppinterface/io_func.hpp
@@ -1,7 +1,7 @@
 #define GCC_VERSION (__GNUC__ * 10000 \
                      + __GNUC_MINOR__ * 100 \
                      + __GNUC_PATCHLEVEL__)
-#if GCC_VERSION < 80000
+#if GCC_VERSION < 80100
 #include <experimental/filesystem>
     namespace fs = std::experimental::filesystem;
 #else
