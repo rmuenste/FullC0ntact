852ebe1955c9ac9b0b3a7bb49486b437908ae466
Raphael Muenster <raphael.muenster@gmail.com>
2018-10-31 15:26:50 +0100

    Updated ctest configuration

---
+++ CTestConfig.cmake
@@ -0,0 +1,10 @@
+
+set(CTEST_PROJECT_NAME "FullC0ntact")
+
+set(CTEST_DROP_METHOD "http")
+
+set(CTEST_DROP_SITE "129.217.165.82/CDash/public")
+
+set(CTEST_DROP_LOCATION "/submit.php?project=Feat_FloWer")
+
+set(CTEST_DROP_SITE_CDASH TRUE)
