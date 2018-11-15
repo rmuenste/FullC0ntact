5b117d3b824a443efa3228c84e8d85b392f62be0
Raphael Muenster <raphael.muenster@gmail.com>
2018-11-15 14:39:34 +0100

    Started implementing elastic hexa

--- applications/cgal_tests/cgal_tests.cpp
+++ applications/cgal_tests/cgal_tests.cpp
@@ -24,27 +24,230 @@
 // Choose a geometry kernel
 typedef CGAL::Simple_cartesian<double> Kernel;
 
-typedef Kernel::Point_3 Point_3;
+// Make a short-hand for the geometry Kernel type
+typedef Kernel::FT FT;
 
-typedef CGAL::Polyhedron_3<Kernel>     Polyhedron;
+// Define short-hands for the other CGAL types that we
+// are going to use in the application
+typedef Kernel::Point_2 Point_2;
+typedef Kernel::Segment_2 Segment_2;
 
-typedef Polyhedron::Vertex_iterator Vertex_iterator;
+typedef Kernel::Point_3 Point;
+typedef Kernel::Triangle_3 Triangle;
+typedef Kernel::Vector_3 Vector;
+
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+
+typedef Kernel::Ray_3 Ray;
+
+typedef CGAL::AABB_face_graph_triangle_primitive<Polyhedron>        Facet_Primitive;
+typedef CGAL::AABB_traits<Kernel, Facet_Primitive>                  Facet_Traits;
+typedef CGAL::AABB_tree<Facet_Traits>                               Facet_tree;
+
+typedef CGAL::AABB_face_graph_triangle_primitive<Polyhedron> Primitive;
+typedef CGAL::AABB_traits<Kernel, Primitive> Traits;
+typedef CGAL::AABB_tree<Traits> Tree;
+typedef Tree::Point_and_primitive_id Point_and_primitive_id;
+
+namespace i3d {
+
+  /*
+   * Function to convert the CGAL class Point to a Vec3
+   */ 
+  inline Vec3 pointToVec3(const Point &p)
+  {
+    return Vec3(p.x(), p.y(), p.z());
+  }
+
+  class MeshTestCGAL : public Application<> {
+
+  public:
+
+    Polyhedron *Polyhedron_;
+
+    MeshTestCGAL() : Application() {
+
+    }
+
+    ~MeshTestCGAL() {};
+
+    double random_in(const double a,
+      const double b)
+    {
+      double r = rand() / (double)RAND_MAX;
+      return (double)(a + (b - a) * r);
+    }
+
+    /*
+     * This function generates and returns a
+     * random 3d vector with components x,y,z in [0,1]
+     */ 
+    Vector random_vector()
+    {
+      double x = random_in(0.0, 1.0);
+      double y = random_in(0.0, 1.0);
+      double z = random_in(0.0, 1.0);
+      return Vector(x, y, z);
+    }
+
+    /*
+     * The init function of a FullC0ntact application. The required
+     * data structures are initialized here.
+     * @param fileName File name of the application data file
+     *
+     */
+    void init(std::string fileName) {
+
+      size_t pos = fileName.find(".");
+      std::string ending = fileName.substr(pos);
+
+      std::transform(ending.begin(), ending.end(), ending.begin(), ::tolower);
+      if (ending == ".xml")
+      {
+
+        FileParserXML myReader;
+
+        //Get the name of the mesh file from the
+        //configuration data file.
+        myReader.parseDataXML(this->dataFileParams_, fileName);
+
+      }//end if
+      else
+      {
+        std::cerr << "Invalid data file ending: " << ending << std::endl;
+        exit(1);
+      }//end else
+
+      if (dataFileParams_.hasExtents_)
+      {
+        grid_.initCube(dataFileParams_.extents_[0], dataFileParams_.extents_[2],
+          dataFileParams_.extents_[4], dataFileParams_.extents_[1],
+          dataFileParams_.extents_[3], dataFileParams_.extents_[5]);
+      }
+      else
+        grid_.initCube(xmin_, ymin_, zmin_, xmax_, ymax_, zmax_);
+
+    }
+
+    /*
+     * In this method we write out the calculated results into
+     * a file that can be visualized by ParaView
+     */
+    void writeGrid(int out)
+    {
+
+      std::ostringstream sNameGrid;
+      std::string sGrid("output/grid.vtk");
+
+      sNameGrid << "." << std::setfill('0') << std::setw(5) << 0;
+      sGrid.append(sNameGrid.str());
+
+      CVtkWriter writer;
+      writer.WriteUnstr(grid_, sGrid.c_str());
+
+    }
+
+    void run() {
+
+      grid_.initStdMesh();
+
+      grid_.calcVol();
+
+      ElementIter e_it = grid_.elem_begin();
+
+      for(; e_it != grid_.elem_end(); e_it++) {
+
+        int index = e_it.idx();
+        std::cout << index << ")Hexa volume: " << grid_.elemVol_[index] << std::endl;
+
+        Hexa &hexa = *e_it;
+
+        UnstructuredGrid<Real, DTraits>::VertElemIter ve_it = grid_.VertElemBegin(&hexa);
+
+        for(; ve_it != grid_.VertElemEnd(&hexa); ve_it++) {
+
+          std::cout << ve_it.GetInt() << " = VertexId " << std::endl;
+
+        }
+
+      }
+
+      std::cout << "Total mesh volume: " << grid_.vol_ << std::endl;
+
+//      elementsAtVertexIdx_[i]+=elementsAtVertexIdx_[i-1];
+//
+//      elementsAtVertex_ = new int[isize];
+
+      // Write out the computed result in VTK ParaView format 
+      writeGrid(0);
+
+    }
+
+  };
+
+}
 
 int main()
 {
+  using namespace i3d;
 
-  Point_3 p( 1.0, 0.0, 0.0);
-  Point_3 q( 0.0, 1.0, 0.0);
-  Point_3 r( 0.0, 0.0, 1.0);
-  Point_3 s( 0.0, 0.0, 0.0);
+  MeshTestCGAL myApp;
 
-  Polyhedron P;
+  myApp.init(std::string("start/sampleRigidBody.xml"));
 
-  P.make_tetrahedron(p, q, r, s);
-
-  CGAL::set_ascii_mode(std::cout);
-
-  std::copy(P.points_begin(), P.points_end(), std::ostream_iterator<Point_3>(std::cout, "\n"));
+  myApp.run();
 
   return EXIT_SUCCESS;
 }
+
+//#include <application.h>
+//#include <reader.h>
+//#include <distancemap.h>
+//#include <meshobject.h>
+//#include <iostream>
+//#include <fstream>
+//#include <vtkwriter.h>
+//#include <iomanip>
+//#include <sstream>
+//
+///*
+// * Includes for CGAL
+// */
+//#include <CGAL/Simple_cartesian.h>
+//#include <CGAL/AABB_tree.h>
+//#include <CGAL/AABB_traits.h>
+//#include <CGAL/config.h>
+//#include <CGAL/Polyhedron_3.h>
+//
+//#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
+//#include <CGAL/AABB_halfedge_graph_segment_primitive.h>
+//#include <CGAL/AABB_face_graph_triangle_primitive.h>
+//
+//// Choose a geometry kernel
+//typedef CGAL::Simple_cartesian<double> Kernel;
+//
+//typedef Kernel::Point_3 Point_3;
+//
+//typedef CGAL::Polyhedron_3<Kernel>     Polyhedron;
+//
+//typedef Polyhedron::Vertex_iterator Vertex_iterator;
+//
+//int main()
+//{
+//
+//  Point_3 p( 1.0, 0.0, 0.0);
+//  Point_3 q( 0.0, 1.0, 0.0);
+//  Point_3 r( 0.0, 0.0, 1.0);
+//  Point_3 s( 0.0, 0.0, 0.0);
+//
+//  Polyhedron P;
+//
+//  P.make_tetrahedron(p, q, r, s);
+//
+//  CGAL::set_ascii_mode(std::cout);
+//
+//  std::copy(P.points_begin(), P.points_end(), std::ostream_iterator<Point_3>(std::cout, "\n"));
+//
+//  return EXIT_SUCCESS;
+//}
+//
