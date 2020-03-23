b711c684c83732f1cf8233bee378daea0db8a956
Raphael Muenster <raphael.muenster@gmail.com>
2020-03-23 10:29:35 +0100

    WIP: boundary layer user inputs

--- unit_tests/boundarylayer_generation/boundary_layer_generation.py
+++ unit_tests/boundarylayer_generation/boundary_layer_generation.py
@@ -2,7 +2,44 @@ import sys
 import os
 import getopt
 import subprocess
+import glob
 #from unv_io import stitchLayers2Unv
+
+#===============================================================================
+#                             deleteIfExists
+#===============================================================================
+def deleteIfExists(file):
+    if os.path.exists(file):
+        os.remove(file)
+    else:
+        print("The file does not exist")
+
+#===============================================================================
+#                                makeClean 
+#===============================================================================
+def makeClean():
+    """
+      Clean the working directory
+    """
+    stlFiles = glob.glob('*.stl')
+    vtkFiles = glob.glob('*.vtk')
+    offFiles = glob.glob('*.off')
+    datFiles = glob.glob('*.dat')
+
+    for item in stlFiles:
+        deleteIfExists(item)
+
+    for item in vtkFiles:
+        deleteIfExists(item)
+
+    for item in offFiles:
+        deleteIfExists(item)
+
+    for item in datFiles:
+        deleteIfExists(item)
+
+    deleteIfExists("outer.unv")
+    deleteIfExists("myout.unv")
 
 #===============================================================================
 #                        usage function
@@ -17,22 +54,26 @@ def usage():
     print("[-b', '--base-layer]: path to the .off file of the first boundary layer")
     print("[-r', '--orig-mesh]: path to the .dat file of the layer to which we want to attach the boundary layers")
     print("[-o', '--output-file]: name of the output unv file")
+    print("[-s', '--salome-path]: path to the salome launcher")
+    print("[-c', '--clean]: remote temporary and intermediate files from the working directory")
+    print("[-t', '--thickness]: thickness of a boundary layer")
+    print("[-l', '--layers]: the total number of boundary layers")
     print("[-h', '--help']: prints this message")
     print("Example: python3 ./unv_io.py -u netzsch_outer2.unv -b baseMeshLayer1.off -r RotorI3.dat")
 
 def main():
-    print("Hello World!")
 
     unvMesh = ""
     origMesh = ""
     baseLayer = ""
+    numLayers = 2
     outputFile = "output.unv"
     salomePath = "/home/rafa/bin/SALOME-9.3.0-UB18.04-SRC/salome"
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'u:r:b:t:m:o:s:h',
+        opts, args = getopt.getopt(sys.argv[1:], 'u:r:b:t:m:o:s:l:ch',
                                    ['unv-mesh=', 'orig-mesh=', 'base-layer=', 'thickness=', 'method=', 'output-file=',
-                                    'salome-path=',
+                                    'salome-path=', 'layers=', 'clean',
                                     'help'])
 
     except getopt.GetoptError:
@@ -57,6 +98,11 @@ def main():
             outputFile = arg
         elif opt in ('-s', '--salome-path'):
             salomePath = arg
+        elif opt in ('-l', '--layers'):
+            numLayers = int(opt)
+        elif opt in ('-c', '--clean'):
+            makeClean()
+            sys.exit(2)
         else:
             usage()
             sys.exit(2)
@@ -65,7 +111,6 @@ def main():
     unvMeshAbsolute = os.getcwd() + '/' + unvMesh
     unvMeshOutAbsolute = os.getcwd() + '/start.unv' 
     workingDir = os.getcwd() 
-    print(unvMeshAbsolute)
 
     subprocess.call(['%s --shutdown-servers=1 -t salome_command_dump.py args:%s,%s,%s' % (salomePath, unvMeshAbsolute, unvMeshOutAbsolute, workingDir)], shell=True)
     subprocess.call(['python3 ./dat2off.py -i StatorI.dat -o statori.off'], shell=True)
